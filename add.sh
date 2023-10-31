2ef9b11e34832eca24a1e21db3a26439d84cc39b
sandeep12983 <vu1f1920056@pvppcoe.ac.in>
2023-10-31 11:13:22 +0530

    first commit

---
+++ add.sh
@@ -0,0 +1,4 @@
+read -p "Enter 1st Num : " a
+read -p "Enter 2nd Num : " b
+x=$(( $a + $b ))
+echo $a "+" $b "=" $x
---
+++ if.sh
@@ -0,0 +1,6 @@
+read -p "Enter first num: " a
+read -p "enter second num: " b
+if [ $a -gt $b ]
+then
+echo $a in greater than $b
+fi
